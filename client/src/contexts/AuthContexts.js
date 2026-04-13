0e38fe4940ca566a83d8a5a8011cb860edfbedf5
kaiskidex <166215444+kaiskidex@users.noreply.github.com>
2026-04-14 01:30:31 +0800

    fixed paths

--- client/src/contexts/AuthContexts.js
+++ client/src/contexts/AuthContexts.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
-import api from './services/api';
+import api from '../services/api';
 
 const API_URL = api.defaults.baseURL;
 const AuthContext = createContext();
